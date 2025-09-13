65876a3215b9375d6b606e3fedaf12d7146fe78b
cakes <cakes@haiiro.moe>
2025-09-14 00:50:11 +0200

    btw

--- modules/shells/fish.nix
+++ modules/shells/fish.nix
@@ -11,6 +11,8 @@
     shellAliases = {
       ritual = "sudo nixos-rebuild switch --flake $HOME/NixOS#coven";
       cp = "/run/current-system/sw/bin/cp --reflink=auto -v";
+      rm = "trash --trash-dir ${config.home.homeDirectory}/Trash";
+      btw = "echo i use nix btw :3";
     };
   };
 }
