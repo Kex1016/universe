230beda1bbbf0a2a605d64dfb717c626d4b91c8e
cakes <cakes@haiiro.moe>
2025-09-13 16:55:41 +0200

    replace steam.nix with gaming.nix

--- modules/apps/gaming.nix
+++ modules/apps/gaming.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 
 {
+  home.packages = with pkgs; [
+    prismlauncher
+    pkgs-unstable.r2modman
+  ];
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
