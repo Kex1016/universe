094558480f23091214e7f3cbc541b0f2198e7da1
cakes <cakes@haiiro.moe>
2025-09-13 16:57:37 +0200

    i forgot that steam.nix is a system module

--- modules/apps/gaming.nix
+++ modules/apps/gaming.nix
@@ -1,20 +1,12 @@
-{ config, pkgs, pkgs-unstable, ... }:
-
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 {
   home.packages = with pkgs; [
     prismlauncher
     pkgs-unstable.r2modman
   ];
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    protontricks = {
-      enable = true;
-    };
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
-  };
 }
---
+++ modules/apps/steam.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    protontricks = {
+      enable = true;
+    };
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
+}
