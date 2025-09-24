240fe67e1b7206cb26adfe3f483db8340a5c53de
cakes <cakes@haiiro.moe>
2025-09-24 22:38:56 +0200

    proper update aliases

--- modules/shells/fish.nix
+++ modules/shells/fish.nix
@@ -9,7 +9,9 @@
     enable = true;
     functions = { fish_greeting = "fastfetch"; };
     shellAliases = {
-      ritual = "sudo nixos-rebuild switch --flake $HOME/NixOS#coven";
+      convene = "cd $HOME/NixOS && nix flake update";
+      sacrifice = "sudo nixos-rebuild switch --flake $HOME/NixOS#coven";
+      ritual = "convene && sacrifice";
       cp = "/run/current-system/sw/bin/cp --reflink=auto -v";
       rm = "trash --trash-dir ${config.home.homeDirectory}/Trash";
       btw = "echo i use nix btw :3";
