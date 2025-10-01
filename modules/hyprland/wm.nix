b0636dd81ccb6ecc6876d4c96c16a2e08b178c80
Kex1016 <cakes@haiiro.moe>
2025-10-01 13:19:22 +0200

    replace dunst, swayosd and waybar with hyprpanel
    for now this is the default config

--- modules/hyprland/wm.nix
+++ modules/hyprland/wm.nix
@@ -339,7 +339,7 @@ in
         follow = "mouse";
       };
     };
-  };
+  }; */
 
   services.udiskie = {
     enable = true;
@@ -360,9 +360,46 @@ in
     enable = true;
   };
 
-  services.swayosd = {
+  /* services.swayosd = {
     enable = true;
     topMargin = 0.9;
+  }; */
+
+  programs.hyprpanel = {
+    enable = true;
+    package = pkgs-unstable.hyprpanel;
+    settings = {
+      layout = {
+        bar.layouts = {
+          "0" = {
+            left = [ "dashboard" "workspaces" ];
+            middle = [ "media" ];
+            right = [ "volume" "systray" "notifications" ];
+          };
+        };
+      };
+
+      bar.launcher.autoDetectIcon = true;
+      bar.workspaces.show_icons = true;
+
+      menus.clock = {
+        time = {
+          military = true;
+          hideSeconds = true;
+        };
+        weather.unit = "metric";
+      };
+
+      menus.dashboard.directories.enabled = false;
+      menus.dashboard.stats.enable_gpu = true;
+
+      theme.bar.transparent = true;
+
+      theme.font = {
+        name = "RobotoMono Nerd Font";
+        size = "16px";
+      };
+    };
   };
 
   services.hyprpolkitagent = {
