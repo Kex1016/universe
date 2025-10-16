d0f18192efe51b7a0ee0fe59c1b91d860b7b7b81
cakes <cakes@haiiro.moe>
2025-10-16 19:40:28 +0200

    add appimage support

--- system.nix
+++ system.nix
@@ -95,6 +95,15 @@
     ]; # Enable ‘sudo’ for the user.
   };
 
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
+
   nix.gc = {
     automatic = true;
     dates = "weekly";
