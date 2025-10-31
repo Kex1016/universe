818b8beb2ce44d2fb8557a25d2396267866ef6bb
cakes <cakes@haiiro.moe>
2025-10-31 12:26:03 +0100

    add an nfs mount and ollama

--- modules/setups/coven/system.nix
+++ modules/setups/coven/system.nix
@@ -8,5 +8,24 @@
     ../../apps/steam.nix
   ];
 
+  fileSystems."/nas/m" = {
+    device = "192.168.1.5:/volume1/docker/media/jelly/mus";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+    ];
+  };
+
   networking.hostName = "coven";
+
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+  };
+
+  services.open-webui = {
+    enable = true;
+    port = 9090;
+  };
 }
