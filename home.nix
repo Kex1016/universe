34bbbb072089448cc92365543af23cdfcc9a83aa
cakes <cakes@haiiro.moe>
2025-11-24 13:22:50 +0100

    add more mimetypes

--- home.nix
+++ home.nix
@@ -75,6 +75,22 @@
       "application/vnd.apple.mpegurl" = "mpv.desktop";
       "video/x-f4v" = "mpv.desktop";
       "video/x-smv" = "mpv.desktop";
+      "audio/mpeg" = "com.github.neithern.g4music.desktop";
+"audio/mp4" = "com.github.neithern.g4music.desktop";
+"audio/ogg" = "com.github.neithern.g4music.desktop";
+"audio/webm" = "com.github.neithern.g4music.desktop";
+"audio/flac" = "com.github.neithern.g4music.desktop";
+"audio/wav" = "com.github.neithern.g4music.desktop";
+"audio/aac" = "com.github.neithern.g4music.desktop";
+"audio/x-aiff" = "com.github.neithern.g4music.desktop";
+"audio/x-m4a" = "com.github.neithern.g4music.desktop";
+"audio/x-ms-wma" = "com.github.neithern.g4music.desktop";
+"audio/x-wavpack" = "com.github.neithern.g4music.desktop";
+"audio/x-ape" = "com.github.neithern.g4music.desktop";
+"audio/x-mod" = "com.github.neithern.g4music.desktop";
+"audio/x-it" = "com.github.neithern.g4music.desktop";
+"audio/x-s3m" = "com.github.neithern.g4music.desktop";
+
     };
   };
   services.gnome-keyring.enable = true;
