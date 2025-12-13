16c60ca854065b729f5c9038b11e89a71a7ce605
cakes <cakes@haiiro.moe>
2025-12-13 17:34:47 +0100

    add the generics to cosmic too

--- modules/wms/cosmic/home.nix
+++ modules/wms/cosmic/home.nix
@@ -2,6 +2,9 @@
 {
   imports = [ 
     ../../shells/fish.nix
+    ../generic/home/kitty.nix
+    ../generic/home/symlinks.nix
+    ../generic/home/userDirs.nix
     ../generic/home/symlinks.nix
     ../generic/home/userDirs.nix
   ];
--- modules/wms/cosmic/system.nix
+++ modules/wms/cosmic/system.nix
@@ -1,6 +1,11 @@
 { ... }:
 {
   imports = [
+    ../generic/system/ananicy.nix
+    ../generic/system/boot.nix
+    ../generic/system/fonts.nix
+    ../generic/system/gaming.nix
+    ../generic/system/updater.nix
     ./system/login.nix
     ./system/wm.nix
   ];
