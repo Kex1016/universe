74d67688e1e3dcaf026708085b67049740abbb6e
cakes <cakes@haiiro.moe>
2025-12-25 13:50:57 +0100

    delete more hypr components

--- modules/apps/hyprland/home/nemo.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  home.packages = with pkgs; [
-    nemo-with-extensions
-    file-roller # this is here because its very relevant to the above pkg
-    nemo-preview
-  ];
-}
--- modules/apps/hyprland/system.nix
+++
@@ -1,4 +0,0 @@
-{ ... }:
-{
-  imports = [ ../generic/system/steam.nix ];
-}
