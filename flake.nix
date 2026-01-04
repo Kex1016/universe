20a1152b8c0381521566f89a64b974943ce0deda
cakes <cakes@haiiro.moe>
2026-01-04 13:40:48 +0100

    vesktop overlay
    why is this not in unstable yet bruh
    - me before learning this issue is literally a couple hours old

--- flake.nix
+++ flake.nix
@@ -74,7 +74,29 @@
         config = {
           allowUnfree = true;
         };
-        overlays = [ inputs.niri-flake.overlays.niri ];
+        overlays = [
+          inputs.niri-flake.overlays.niri
+          (final: prev: {
+            vesktop = prev.vesktop.overrideAttrs (old: {
+              preBuild = ''
+                cp -r ${prev.electron.dist} electron-dist
+                chmod -R u+w electron-dist
+              '';
+              buildPhase = ''
+                runHook preBuild
+
+                pnpm build
+                pnpm exec electron-builder \
+                  --dir \
+                  -c.asarUnpack="**/*.node" \
+                  -c.electronDist="electron-dist" \
+                  -c.electronVersion=${prev.electron.version}
+
+                runHook postBuild
+              '';
+            });
+          })
+        ];
       };
 
       ffaddons = pkgs.callPackage firefox-addons { };
