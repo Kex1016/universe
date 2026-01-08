6276aa1ea341bb2948e61438738252cf23e7c2a9
cakes <cakes@haiiro.moe>
2026-01-08 22:07:32 +0100

    add kagi and make it default
    we shillin

--- modules/apps/generic/home/browsers.nix
+++ modules/apps/generic/home/browsers.nix
@@ -62,8 +62,25 @@
     ];
     profiles.default.search = {
       force = true; # Needed for nix to overwrite search settings on rebuild
-      default = "ddg"; # Aliased to duckduckgo, see other aliases in the link above
+      default = "kagi"; # Aliased to duckduckgo, see other aliases in the link above
       engines = {
+        kagi = {
+          name = "Kagi";
+          urls = [
+            {
+              template = "https://kagi.com/search?q={searchTerms}";
+              params = [
+                {
+                  name = "query";
+                  value = "searchTerms";
+                }
+              ];
+            }
+          ];
+
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = [ "@nx" ]; # Keep in mind that aliases defined here only work if they start with "@"
+        };
         # My nixos Option and package search shortcut
         mynixos = {
           name = "My NixOS";
